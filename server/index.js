6ddc10462075283bbbd9fa8fca4871bbf1af9be6
JamesJunJieHuang <108486354+JamesJunJieHuang@users.noreply.github.com>
2023-01-29 22:22:26 -0500

    updated package json, fixed imports

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import { ppid } from "process"
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import register from "./controllers/auth.js"
+import {register} from "./controllers/auth.js"
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from "./middleware/auth.js"
 
